b3022e22f703cd29d97d026601d07adcdf2e1387
trantrongthanhtam <trantrongthanhtam@gmail.com>
2020-07-17 08:42:46 +0700

    Change index contents

--- pages/index.js
+++ pages/index.js
@@ -10,42 +10,14 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Gõ dấu tiếng việt <a href="https://nextjs.org">Next.js!</a>
+          Chào mừng đến Blog của Nguyễn Thu Thảo!
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Trang blog này là nơi tôi chia sẻ cho các bạn những kiến thức của tôi về mọi lĩnh vực đời sống mà tôi đúc kết được. Đây cũng là nơi giãy bày những tâm sự, mách nhỏ cho bạn đọc...
         </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        
       </main>
 
       <footer>
@@ -54,8 +26,8 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+          Powered by Tam Tran and{' '}
+  <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
 
@@ -76,6 +48,7 @@ export default function Home() {
           flex-direction: column;
           justify-content: center;
           align-items: center;
+          max-width: 1000px;
         }
 
         footer {
