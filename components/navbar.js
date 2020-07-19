b06c5977c8518645647141343070ff8b8cfe0196
Tam Tran <trantrongthanhtam@gmail.com>
2020-07-19 19:30:25 +0700

    minor change

--- components/navbar.js
+++ components/navbar.js
@@ -8,33 +8,34 @@ export default function NavBar() {
       className="mr-auto ml-auto"
       fixed="top"
       expand="md"
-      style={{ fontWeight: "bold", maxWidth: "1500px" }}
+      style={{ fontWeight: "bold", maxWidth: "1500px"}}
     >
       <Navbar.Brand href="#"> Nguyễn Thu Thảo</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#" className="mr-auto ml-auto">
-            Home
-          </Nav.Link>
-          <Nav.Link href="#" className="mr-auto ml-auto">
-            About
-          </Nav.Link>
-          <Nav.Link href="#" className="mr-auto ml-auto">
-            Blog
-          </Nav.Link>
-        </Nav>
-        <Form style={{ display: "flex", flexDirection: "row" }}>
+        
+        <Form className="ml-auto mr-auto" style={{ display: "flex", flexDirection: "row"}}>
           <FormControl
             type="text"
             placeholder="Search"
-            className="mr-sm-2"
-            style={{ marginRight: "1rem" }}
+            className="mr-md-2"
+            style={{ marginRight:"1rem"}}
           />
           <Button className="ml-sm-2" variant="outline-dark">
             Search
           </Button>
         </Form>
+        <Nav >
+          <Nav.Link href="#" className="mr-auto ml-auto">
+            Trang chủ
+          </Nav.Link>
+          <Nav.Link href="#" className="mr-auto ml-auto">
+            Giới thiệu
+          </Nav.Link>
+          <Nav.Link href="#" className="mr-auto ml-auto">
+            Blog
+          </Nav.Link>
+        </Nav>
       </Navbar.Collapse>
     </Navbar>
   );
